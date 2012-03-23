1f0f4ed177cbdb3ebcdf6897fc81d546f5f58738
brunabas22@gmail.com <brunabas22@gmail.com@7829f8ed-64c2-7fa1-ecdd-565c78c83bf4>
2012-03-23 20:52:40 +0000

    Projeto BD

---
+++ ProjetoBD/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0;URL=Views/indice.html">
+        <title></title>
+    </head>
+    <body>
+        
+        <?php
+        //phpinfo();<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        ?>
+    </body>
+</html>
